a4362d1c51e87fc8d875c023a7926e80b969ab75
arturha4 <sadbooys.2001@gmail.com>
2025-07-29 11:43:15 +0500

    added db, orm, models, async tasks

--- config.py
+++ config.py
@@ -1,5 +1,23 @@
-PG_USER = 'postgres'
-PG_PASSWORD = 'postgres'
-PG_DB = 'postgres'
-PG_HOST = 'localhost'
-PG_PORT = 5432
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    TELEGRAM_BOT_TOKEN: str
+    TELEGRAM_BOT_ADMIN_ID: str
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env')
+    )
+
+    def get_db_url(self):
+        return (f'postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@'
+                f'{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}')
+
+settings = Settings()
