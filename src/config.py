545616ca4b3b0877e738d8340f03f43f2c623d6e
arturha4 <sadbooys.2001@gmail.com>
2025-08-05 14:31:41 +0500

    code cleanup

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-#postgresql+asyncpg://postgres:postgres@db:5432/postgres
+
 
 class Settings(BaseSettings):
+    # postgresql+asyncpg://postgres:postgres@db:5432/postgres
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
@@ -9,6 +10,7 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     TELEGRAM_BOT_TOKEN: str
     TELEGRAM_BOT_ADMIN_ID: str
+    BROKER_URL: str
 
     model_config = SettingsConfigDict(
         env_file='.docker-env'
@@ -21,5 +23,6 @@ class Settings(BaseSettings):
     def get_db_url_migrations(self):
         return (f'postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@'
                 f'{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}')
+
 
 settings = Settings()
